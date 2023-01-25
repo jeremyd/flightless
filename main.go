9b20b8a8dcb2632e02a2dd661c6b2b74b9a7e6fb
jeremyd <jeremyd>
2023-01-25 10:27:06 -0800

    works aw yea!

--- main.go
+++ main.go
@@ -19,17 +19,24 @@ import (
 )
 
 type Metadata struct {
-	PubkeyHex   string      `gorm:"primaryKey;size:256"`
-	Name        string      `gorm:"size:1024"`
-	About       string      `gorm:"size:4096"`
-	Nip05       string      `gorm:"size:512"`
-	Lud06       string      `gorm:"size:2048"`
-	Lud16       string      `gorm:"size:512"`
-	Website     string      `gorm:"size:512"`
-	DisplayName string      `gorm:"size:512"`
-	Picture     string      `gorm:"type:text;size:65535"`
-	UpdatedAt   time.Time   `gorm:"autoUpdateTime"`
-	Follows     []*Metadata `gorm:"many2many:metadata_follows"`
+	PubkeyHex   string `gorm:"primaryKey;size:256"`
+	Name        string `gorm:"size:1024"`
+	About       string `gorm:"size:4096"`
+	Nip05       string `gorm:"size:512"`
+	Lud06       string `gorm:"size:2048"`
+	Lud16       string `gorm:"size:512"`
+	Website     string `gorm:"size:512"`
+	DisplayName string `gorm:"size:512"`
+	Picture     string `gorm:"type:text;size:65535"`
+	//UpdatedAt   time.Time   `gorm:"autoUpdateTime"`
+	RecommendServer string      `gorm:"size:512"`
+	Follows         []*Metadata `gorm:"many2many:metadata_follows"`
+}
+
+type Proof struct {
+	ID      uint   `gorm:"primaryKey"`
+	Relay   string `gorm:"size:512"`
+	EventID string `gorm:"size:512"`
 }
 
 func main() {
@@ -57,7 +64,10 @@ func main() {
 
 	// connect to relay(s)
 	relayUrls := []string{
+		"wss://relay.snort.social",
+		"wss://relay.damus.io",
 		"wss://nostr.zebedee.cloud",
+		"wss://eden.nostr.land",
 		"wss://nostr-pub.wellorder.net",
 		"wss://nostr-dev.wellorder.net",
 	}
@@ -120,20 +130,33 @@ func main() {
 					}
 					//fmt.Printf("name: %s\nabout: %s\n\n", m.Name, m.About)
 					m.PubkeyHex = ev.PubKey
-					m.UpdatedAt = ev.CreatedAt
+					//m.UpdatedAt = ev.CreatedAt
 					if len(m.Picture) > 65535 {
 						fmt.Println("dumbass put too big a picture, skipping")
 						continue
 					}
 					rowsUpdated := db.Model(Metadata{}).Where("pubkey_hex = ?", m.PubkeyHex).Updates(&m).RowsAffected
 					if rowsUpdated == 0 {
-						err := db.Create(&m).Error
+						err := db.Save(&m).Error
 						if err != nil {
 							fmt.Println(err)
 						}
 						fmt.Printf("Created metadata for %s, %s\n", m.Name, m.Nip05)
 					} else {
 						fmt.Printf("Updated metadata for %s, %s\n", m.Name, m.Nip05)
+					}
+					// recommend relay
+				} else if ev.Kind == 2 {
+					m := Metadata{PubkeyHex: ev.PubKey, RecommendServer: ev.Content}
+					rowsUpdated := db.Model(Metadata{}).Where("pubkey_hex = ?", m.PubkeyHex).Updates(&m).RowsAffected
+					if rowsUpdated == 0 {
+						err := db.Save(&m).Error
+						if err != nil {
+							fmt.Println(err)
+						}
+						fmt.Printf("Created metadata for %s, %s, %s", m.Name, m.Nip05, m.RecommendServer)
+					} else {
+						fmt.Printf("Updated metadata for %s, %s, %s\n", m.Name, m.Nip05, m.RecommendServer)
 					}
 					// Contact Lists
 				} else if ev.Kind == 3 {
@@ -143,37 +166,44 @@ func main() {
 					allPTags := ev.Tags.GetAll(pTags)
 					var person Metadata
 					notFoundError := db.First(&person, "pubkey_hex = ?", ev.PubKey).Error
-
 					if notFoundError != nil {
 						fmt.Printf("Creating blank metadata for %s\n", ev.PubKey)
 						person = Metadata{
 							PubkeyHex: ev.PubKey,
 						}
 						db.Create(&person)
+						//db.Omit("Follows").Create(&person)
 					} else {
-						fmt.Printf("updating follows for %s: %s\n", person.Name, person.PubkeyHex)
+						fmt.Printf("updating (%d) follows for %s: %s\n", len(allPTags), person.Name, person.PubkeyHex)
 					}
 					db.Model(&person).Association("Follows").Clear()
+					db.Save(&person)
 					for _, c := range allPTags {
 						var followPerson Metadata
 						notFoundFollow := db.First(&followPerson, "pubkey_hex = ?", c[1]).Error
 						if notFoundFollow != nil {
 							// follow user not found, need to create it
 							newUser := Metadata{PubkeyHex: c[1]}
-							createNewErr := db.Create(&newUser).Error
+							//createNewErr := db.Create(&newUser).Error
+							//createNewErr := db.Omit(clause.Associations).Create(&newUser).Error
+							createNewErr := db.Omit("Follows").Create(&newUser).Error
 							if createNewErr != nil {
 								fmt.Println("Error creating user for follow: ", createNewErr)
 							}
-							db.Model(&person).Association("Follows").Append(&newUser)
+							// use gorm insert statement to update the join table
+							db.Exec("insert into metadata_follows (metadata_pubkey_hex, follow_pubkey_hex) values (?, ?)", person.PubkeyHex, newUser.PubkeyHex)
 						} else {
 							// follow user found,
-							db.Model(&person).Association("Follows").Append(&followPerson)
+							// use gorm insert statement to update the join table
+							db.Exec("insert into metadata_follows (metadata_pubkey_hex, follow_pubkey_hex) values (?, ?)", person.PubkeyHex, followPerson.PubkeyHex)
 						}
 					}
-					updatedPerson := Metadata{PubkeyHex: ev.PubKey}
-					var newFollows []Metadata
-					db.Model(&updatedPerson).Association("Follows").Find(&newFollows)
-					fmt.Printf("%d newFollows for %s\n", len(newFollows), updatedPerson.PubkeyHex)
+					/*
+						updatedPerson := Metadata{PubkeyHex: ev.PubKey}
+						var newFollows []Metadata
+						db.Model(&updatedPerson).Association("Follows").Find(&newFollows)
+						fmt.Printf("%d newFollows for %s\n", len(newFollows), updatedPerson.PubkeyHex)
+					*/
 				}
 			}
 		}()
