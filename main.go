e959ab8b070c424fc16acc3022af893d3c4cefc9
jeremyd <jeremyd>
2023-01-27 14:09:55 -0800

    super working aw yea

--- main.go
+++ main.go
@@ -14,48 +14,85 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
-
-	generic_sync "github.com/SaveTheRbtz/generic-sync-map-go"
 )
 
 type Metadata struct {
-	PubkeyHex       string      `gorm:"primaryKey;size:256"`
-	Name            string      `gorm:"size:1024"`
-	About           string      `gorm:"size:4096"`
-	Nip05           string      `gorm:"size:512"`
-	Lud06           string      `gorm:"size:2048"`
-	Lud16           string      `gorm:"size:512"`
-	Website         string      `gorm:"size:512"`
-	DisplayName     string      `gorm:"size:512"`
-	Picture         string      `gorm:"type:text;size:65535"`
-	UpdatedAt       time.Time   `gorm:"autoUpdateTime"`
-	RecommendServer string      `gorm:"size:512"`
-	Follows         []*Metadata `gorm:"many2many:metadata_follows"`
+	PubkeyHex   string            `gorm:"primaryKey;size:256"`
+	Name        string            `gorm:"size:1024"`
+	About       string            `gorm:"size:4096"`
+	Nip05       string            `gorm:"size:512"`
+	Lud06       string            `gorm:"size:2048"`
+	Lud16       string            `gorm:"size:512"`
+	Website     string            `gorm:"size:512"`
+	DisplayName string            `gorm:"size:512"`
+	Picture     string            `gorm:"type:text;size:65535"`
+	UpdatedAt   time.Time         `gorm:"autoUpdateTime"`
+	Follows     []*Metadata       `gorm:"many2many:metadata_follows"`
+	Servers     []RecommendServer `gorm:"foreignKey:PubkeyHex;references:PubkeyHex"`
 }
 
-type Proof struct {
-	ID      uint   `gorm:"primaryKey"`
-	Relay   string `gorm:"size:512"`
-	EventID string `gorm:"size:512"`
+type RecommendServer struct {
+	PubkeyHex     string    `gorm:"primaryKey;size:256"`
+	Url           string    `gorm:"size:512"`
+	UpdatedAt     time.Time `gorm:"autoUpdateTime"`
+	CreatedAt     time.Time `gorm:"autoUpdateTime"`
+	RecommendedBy string    `gorm:"size:256"`
 }
 
-func doRelay(db *gorm.DB, seenMessages *generic_sync.MapOf[string, bool], ctx context.Context, url string) {
+type RelayStatus struct {
+	Url       string    `gorm:"primaryKey;size:512"`
+	Status    string    `gorm:"size:512"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime"`
+}
+
+type GormErr struct {
+	Number  int    `json:"Number"`
+	Message string `json:"Message"`
+}
+
+func checkAndReportGormError(err error, allowErrors []string) bool {
+	if err != nil {
+		for _, e := range allowErrors {
+			if err.Error() == e {
+				fmt.Println("known error: " + e)
+				return true
+			}
+		}
+		byteErr, _ := json.Marshal(err)
+		var newError GormErr
+		json.Unmarshal((byteErr), &newError)
+		fmt.Println(newError)
+		return false
+	}
+	return true
+}
+
+func updateOrCreateRelayStatus(db *gorm.DB, url string, status string) {
+	rowsUpdated := db.Model(RelayStatus{}).Where("url = ?", url).Updates(&RelayStatus{Url: url, Status: status}).RowsAffected
+	if rowsUpdated == 0 {
+		db.Create(&RelayStatus{Url: url, Status: status})
+	}
+}
+
+func doRelay(db *gorm.DB, ctx context.Context, url string) bool {
 	relay, err := nostr.RelayConnect(ctx, url)
 	if err != nil {
 		fmt.Printf("failed initial connection to relay: %s, %s; skipping relay\n", url, err)
-		return
+		updateOrCreateRelayStatus(db, url, "failed initial connection")
+		return false
 	}
+
+	updateOrCreateRelayStatus(db, url, "connection established")
 
 	// create filters
 	filters := []nostr.Filter{{
-		Kinds: []int{0, 3},
+		Kinds: []int{0, 2, 3},
 		//Tags:  t,
 		// limit = 3, get the three most recent notes
-		Limit: 100,
+		Limit: 10,
 	}}
 
 	// create a subscription and submit to relay
-	// results will be returned on the sub.Events channel
 	sub := relay.Subscribe(ctx, filters)
 
 	go func() {
@@ -68,7 +105,6 @@ func doRelay(db *gorm.DB, seenMessages *generic_sync.MapOf[string, bool], ctx co
 	go func() {
 		<-c
 		fmt.Println("exiting gracefully")
-		//cancel()
 		sub.Unsub()
 		relay.Close()
 		// give other relays time to close
@@ -78,24 +114,14 @@ func doRelay(db *gorm.DB, seenMessages *generic_sync.MapOf[string, bool], ctx co
 
 	go func() {
 		for ev := range sub.Events {
-			seen, _ := seenMessages.Load(ev.ID)
-			if seen {
-				fmt.Println("seen..")
-				continue
-			} else {
-				seenMessages.Store(ev.ID, true)
-			}
-			// Metadata
 			if ev.Kind == 0 {
-				//fmt.Println(ev)
+				// Metadata
 				m := Metadata{}
 				err := json.Unmarshal([]byte(ev.Content), &m)
 				if err != nil {
 					fmt.Println(err)
 				}
-				//fmt.Printf("name: %s\nabout: %s\n\n", m.Name, m.About)
 				m.PubkeyHex = ev.PubKey
-				//m.UpdatedAt = ev.CreatedAt
 				if len(m.Picture) > 65535 {
 					fmt.Println("dumbass put too big a picture, skipping")
 					continue
@@ -110,23 +136,24 @@ func doRelay(db *gorm.DB, seenMessages *generic_sync.MapOf[string, bool], ctx co
 				} else {
 					fmt.Printf("Updated metadata for %s, %s\n", m.Name, m.Nip05)
 				}
-				// recommend relay
 			} else if ev.Kind == 2 {
-				m := Metadata{PubkeyHex: ev.PubKey, RecommendServer: ev.Content}
-				rowsUpdated := db.Model(Metadata{}).Where("pubkey_hex = ?", m.PubkeyHex).Updates(&m).RowsAffected
-				if rowsUpdated == 0 {
-					err := db.Save(&m).Error
-					if err != nil {
-						fmt.Println(err)
-					}
-					fmt.Printf("Created metadata for %s, %s, %s", m.Name, m.Nip05, m.RecommendServer)
+				// recommend relay
+				fmt.Println("FOUND TYPE 2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+				var servers []RecommendServer
+				db.Find(&servers, "pubkey_hex = ? and url = ? and recommended_by = ?", ev.PubKey, ev.Content, ev.PubKey)
+				if len(servers) > 0 {
+					// already recommended, update time fields?
+					fmt.Println("already recommended, skip")
 				} else {
-					fmt.Printf("Updated metadata for %s, %s, %s\n", m.Name, m.Nip05, m.RecommendServer)
+					// add to recommended servers
+					db.Create(&RecommendServer{
+						PubkeyHex:     ev.PubKey,
+						Url:           ev.Content,
+						RecommendedBy: ev.PubKey,
+					})
 				}
-				// Contact Lists
 			} else if ev.Kind == 3 {
-				//fmt.Println(ev)
-				//fmt.Println(ev.Tags)
+				// Contact List
 				pTags := []string{"p"}
 				allPTags := ev.Tags.GetAll(pTags)
 				var person Metadata
@@ -137,38 +164,68 @@ func doRelay(db *gorm.DB, seenMessages *generic_sync.MapOf[string, bool], ctx co
 						PubkeyHex: ev.PubKey,
 					}
 					db.Create(&person)
-					//db.Omit("Follows").Create(&person)
 				} else {
 					fmt.Printf("updating (%d) follows for %s: %s\n", len(allPTags), person.Name, person.PubkeyHex)
 				}
-				db.Model(&person).Association("Follows").Clear()
-				db.Save(&person)
+
+				// purge followers that have been 'unfollowed'
+				var oldFollows []Metadata
+				db.Model(&person).Association("Follows").Find(&oldFollows)
+				for _, oldFollow := range oldFollows {
+					found := false
+					for _, n := range allPTags {
+						if n[1] == oldFollow.PubkeyHex {
+							found = true
+						}
+					}
+					if !found {
+						db.Exec("delete from metadata_follows where metadata_pubkey_hex = ? and follow_pubkey_hex = ?", person.PubkeyHex, oldFollow.PubkeyHex)
+					}
+				}
+
 				for _, c := range allPTags {
 					var followPerson Metadata
 					notFoundFollow := db.First(&followPerson, "pubkey_hex = ?", c[1]).Error
 					if notFoundFollow != nil {
 						// follow user not found, need to create it
-						newUser := Metadata{PubkeyHex: c[1]}
-						//createNewErr := db.Create(&newUser).Error
-						//createNewErr := db.Omit(clause.Associations).Create(&newUser).Error
+						var newUser Metadata
+						// follow user recommend server suggestion if it exists
+						if len(c) >= 3 && c[2] != "" {
+							newUser = Metadata{
+								PubkeyHex: c[1],
+								Servers:   []RecommendServer{{Url: c[2], RecommendedBy: person.PubkeyHex}},
+							}
+						} else {
+							newUser = Metadata{PubkeyHex: c[1]}
+						}
 						createNewErr := db.Omit("Follows").Create(&newUser).Error
 						if createNewErr != nil {
 							fmt.Println("Error creating user for follow: ", createNewErr)
 						}
 						// use gorm insert statement to update the join table
-						db.Exec("insert into metadata_follows (metadata_pubkey_hex, follow_pubkey_hex) values (?, ?)", person.PubkeyHex, newUser.PubkeyHex)
+						errExec := db.Exec("insert ignore into metadata_follows (metadata_pubkey_hex, follow_pubkey_hex) values (?, ?)", person.PubkeyHex, newUser.PubkeyHex).Error
+						checkAndReportGormError(errExec, []string{"1062"})
 					} else {
 						// follow user found,
+						// update the follow user's recommend server suggestion
+						if len(c) >= 3 && c[2] != "" {
+							var servers []RecommendServer
+							db.Find(&servers, "pubkey_hex = ? and url = ? and recommended_by = ?", followPerson.PubkeyHex, c[2], person.PubkeyHex)
+							if len(servers) > 0 {
+								// already recommended, update time fields?
+							} else {
+								// add to recommended servers
+								db.Model(&followPerson).Association("Servers").Append(&RecommendServer{
+									Url:           c[2],
+									RecommendedBy: person.PubkeyHex,
+								})
+							}
+						}
 						// use gorm insert statement to update the join table
-						db.Exec("insert into metadata_follows (metadata_pubkey_hex, follow_pubkey_hex) values (?, ?)", person.PubkeyHex, followPerson.PubkeyHex)
+						errExec := db.Exec("insert ignore into metadata_follows (metadata_pubkey_hex, follow_pubkey_hex) values (?, ?)", person.PubkeyHex, followPerson.PubkeyHex).Error
+						checkAndReportGormError(errExec, []string{"1062"})
 					}
 				}
-				/*
-					updatedPerson := Metadata{PubkeyHex: ev.PubKey}
-					var newFollows []Metadata
-					db.Model(&updatedPerson).Association("Follows").Find(&newFollows)
-					fmt.Printf("%d newFollows for %s\n", len(newFollows), updatedPerson.PubkeyHex)
-				*/
 			}
 		}
 	}()
@@ -177,13 +234,16 @@ func doRelay(db *gorm.DB, seenMessages *generic_sync.MapOf[string, bool], ctx co
 		for cErr := range relay.ConnectionError {
 			if cErr != nil {
 				fmt.Printf("relay: %s connection error: %s\n", relay.URL, cErr)
-				time.Sleep(10 * time.Second)
+				updateOrCreateRelayStatus(db, relay.URL, "connection error: "+cErr.Error())
+				// attempt a re-connection
+				time.Sleep(60 * time.Second)
 				fmt.Printf("reconnecting to %s\n", relay.URL)
-				doRelay(db, seenMessages, ctx, relay.URL)
+				updateOrCreateRelayStatus(db, relay.URL, "reconnecting")
+				doRelay(db, ctx, relay.URL)
 			}
 		}
 	}()
-
+	return true
 }
 
 func main() {
@@ -196,6 +256,7 @@ func main() {
 			Colorful:                  false,        // Disable color
 		},
 	)
+
 	dsn := "jeremy:jeremy@tcp(127.0.0.1:3306)/nono?charset=utf8mb4&parseTime=True&loc=UTC"
 	db, dberr := gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: newLogger})
 	db.Logger.LogMode(logger.Silent)
@@ -204,9 +265,12 @@ func main() {
 	}
 
 	migrateErr := db.AutoMigrate(&Metadata{})
-	if migrateErr != nil {
-		panic("migration failed, aborting")
+	migrateErr2 := db.AutoMigrate(&RelayStatus{})
+	migrateErr3 := db.AutoMigrate(&RecommendServer{})
+	if migrateErr != nil || migrateErr2 != nil || migrateErr3 != nil {
+		panic("one or more migrations failed, aborting")
 	}
+
 	ctx, _ := context.WithCancel(context.Background())
 
 	// connect to relay(s)
@@ -217,15 +281,15 @@ func main() {
 		"wss://eden.nostr.land",
 		"wss://nostr-pub.wellorder.net",
 		"wss://nostr-dev.wellorder.net",
+		"wss://relay.nostr.info",
 	}
 
-	seenMessages := new(generic_sync.MapOf[string, bool])
-
 	for _, url := range relayUrls {
-		doRelay(db, seenMessages, ctx, url)
+		doRelay(db, ctx, url)
 	}
 
 	for {
 		time.Sleep(5 * time.Second)
 	}
+
 }
