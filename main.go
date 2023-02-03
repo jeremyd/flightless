6cdddbf50e7a7c2e5c14e73328172444e986e50f
jeremyd <jeremyd>
2023-02-02 17:04:28 -0800

    v0.0.4

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"gorm.io/gorm/logger"
 )
 
-var AppInfo = "flightless v0.0.3"
+var AppInfo = "flightless v0.0.4"
 
 type Metadata struct {
 	PubkeyHex         string `gorm:"primaryKey;size:65"`
